38dc78163902f5f6bf8b273be7f3fb8f4d989c2b
190736 <190736@e0505-16.JC-21.local>
2021-07-07 16:04:17 +0900

    A04完成

--- JC21PS/src/jp/co/jcps/A04/ParticipantListBean.java
+++ JC21PS/src/jp/co/jcps/A04/ParticipantListBean.java
@@ -9,7 +9,7 @@ import java.util.List;
  */
 public class ParticipantListBean implements Serializable {
 
-	// 活動名a
+	// 活動名
 	private String activityName;
 	// 参加者リスト
 	private List<String> participantList;
